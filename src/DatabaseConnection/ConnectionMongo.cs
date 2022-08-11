b24bca676f38f8321aaf009ae36c72a3c5328179
Camilla Batista <camillabatista1995@gmail.com>
2022-08-10 22:25:08 -0300

    add project database connection

---
+++ src/DatabaseConnection/ConnectionMongo.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+
+namespace DatabaseConnection
+{
+    public class ConnectionMongo
+    {
+        private readonly IMongoDatabase _mongoDatabase;
+        private readonly MongoClientSettings _mongoClientSettings;
+
+        public ConnectionMongo()
+        {
+            _mongoClientSettings = MongoClientSettings.FromConnectionString("mongodb+srv://camis:camis@cluster0.pllljc1.mongodb.net/?retryWrites=true&w=majority");
+            _mongoDatabase = new MongoClient(_mongoClientSettings).GetDatabase(MongoUrl
+                .Create("mongodb+srv://camis:camis@cluster0.pllljc1.mongodb.net/?retryWrites=true&w=majority")
+                .DatabaseName);
+        }
+    }
+}
